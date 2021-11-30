cb4e91a0754698ced4ab54992345b3b7191f7c18
hasbi26 <hasbisimaulansyah@gmail.com>
2021-11-30 13:58:44 +0700

    new

--- process.cpp
+++ process.cpp
@@ -3,14 +3,14 @@
 Process::Process(QObject *parent) : QObject(parent)
 {
 
-    qDebug()<<"test";
+// token github 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
 
     redisClient = new Redis("tcp://127.0.0.1");
 
-    qDebug ()<< redisClient;
+  
 
 
-   auto test = redisClient->hget("status", "start");
+   QString test = redisClient->hget("status", "start");
 
    qDebug ()<<"testt ";test;
 
